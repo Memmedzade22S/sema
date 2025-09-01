0629419a99d4a621e85bff65054c552aeb3767b9
Səma <mammadzadasama@gmail.com>
2025-09-01 13:55:06 +0400

    Update task-8.php

--- task-8.php
+++ task-8.php
@@ -1,7 +1,4 @@
 <?php
-
-$n = (int)readline("Ədəd daxil edin: ");
-$digits = str_split((string)abs($n));
-sort($digits);
-echo "Artan sıra: " . implode('', $digits);
+sort($reqemler_r);
+echo "Reqemler artan sira: " . implode(" ", $reqemler_r) . "\n";
 ?>
