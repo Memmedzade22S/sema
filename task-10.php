647d85bb60b6e5c2795c808a2b63ebabf6c54452
Səma <mammadzadasama@gmail.com>
2025-09-01 13:55:47 +0400

    Update task-10.php

--- task-10.php
+++ task-10.php
@@ -1,8 +1,6 @@
 <?php
-
-$n = (int)readline("Ədəd daxil edin: ");
-$digits = str_split((string)abs($n));
-foreach ($digits as $d) {
-    echo $d . "^2 = " . ($d * $d) . PHP_EOL;
+foreach ($reqemler_r as $reqem) {
+    echo ($reqem * $reqem) . " ";
 }
+echo "\n";
 ?>
