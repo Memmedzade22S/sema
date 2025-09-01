8388200a237fd947965589cf8ee1c617ae71ab51
Səma <mammadzadasama@gmail.com>
2025-09-01 12:03:47 +0400

    Create task-1.php

---
+++ task-1.php
@@ -0,0 +1,11 @@
+<?php
+// task1.php
+$n = (int)readline("İkirəqəmli ədəd daxil edin: ");
+if ($n < 10 || $n > 99) {
+    echo "Xəta: ədəd ikirəqəmli olmalıdır!";
+} else {
+    $tens = intdiv($n, 10);
+    $ones = $n % 10;
+    echo $tens . " " . $ones;
+}
+?>
